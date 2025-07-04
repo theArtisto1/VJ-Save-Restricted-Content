1d73432a06ccdbac4882b15000970ec9ba33c80a
theArtisto1 <k297050010@gmail.com>
2025-07-04 10:25:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", "....."))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "4179ce15aab1f8396c0709347df1976a")
+API_HASH = os.environ.get("API_HASH", "......")
 
 # Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "6013909736"))
+ADMINS = int(os.environ.get("ADMINS", "......"))
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
